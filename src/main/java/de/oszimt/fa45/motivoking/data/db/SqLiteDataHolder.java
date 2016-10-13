31f9c8aedeafaac8b0a9834a9757307dddcfe59e
Børge Grunicke <boerge@grunicke.de>
2016-10-13 12:02:00 +0200

    implmeented som small things

---
+++ src/main/java/de/oszimt/fa45/motivoking/data/db/SqLiteDataHolder.java
@@ -0,0 +1,16 @@
+package de.oszimt.fa45.motivoking.data.db;
+
+import de.oszimt.fa45.motivoking.Day;
+import de.oszimt.fa45.motivoking.data.DataHolder;
+
+import java.util.List;
+
+/**
+ * Created by boerg on 13.10.2016.
+ */
+public class SqLiteDataHolder implements DataHolder {
+    @Override
+    public List<Day> findAllDays() {
+        return null;
+    }
+}
