7dcadb5db8d3f2f4e81c7cbaead9b0851ca9e14e
Børge Grunicke <boerge@grunicke.de>
2016-10-13 12:54:11 +0200

    choosing the componentes implkemented

--- src/main/java/de/oszimt/fa45/motivoking/ui/gui/GraphicalUserInterface.java
+++ src/main/java/de/oszimt/fa45/motivoking/ui/gui/GraphicalUserInterface.java
@@ -1,9 +1,15 @@
 package de.oszimt.fa45.motivoking.ui.gui;
 
 import de.oszimt.fa45.motivoking.ui.UserInterface;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * Created by boerg on 13.10.2016.
  */
-public class GraphicalUserInterface implements UserInterface {
+public class GraphicalUserInterface extends Application implements UserInterface {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
