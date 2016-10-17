cb5f172948543dc6fcb6436ffbb23e2f5bebfdd8
RedCyberSamurai <RedCyberSamurai@users.noreply.github.com>
2016-10-17 15:54:15 +0200

    added program logic placeholders

---
+++ src/main/java/de/oszimt/fa45/motivoking/functionality/ProgramLogic1.java
@@ -0,0 +1,16 @@
+package de.oszimt.fa45.motivoking.functionality;
+
+import de.oszimt.fa45.motivoking.data.DataHolder;
+
+/**
+ * Created by RedCyberSamurai on 17.10.2016.
+ */
+public class ProgramLogic1 implements ProgramLogic{
+    private DataHolder m_dataHolder;
+
+    public ProgramLogic1(DataHolder t_dataHolder) {
+        m_dataHolder = t_dataHolder;
+    }
+
+    
+}
---
+++ src/main/java/de/oszimt/fa45/motivoking/functionality/ProgramLogic2.java
@@ -0,0 +1,14 @@
+package de.oszimt.fa45.motivoking.functionality;
+
+import de.oszimt.fa45.motivoking.data.DataHolder;
+
+/**
+ * Created by RedCyberSamurai on 17.10.2016.
+ */
+public class ProgramLogic2 implements ProgramLogic {
+    private DataHolder m_dataHolder;
+
+    public ProgramLogic2(DataHolder t_dataHolder) {
+        m_dataHolder = t_dataHolder;
+    }
+}
