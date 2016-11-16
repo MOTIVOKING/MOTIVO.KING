b5f290b4f7f986534e52d5be07939455ca6ec98a
Børge Grunicke <boerge@grunicke.de>
2016-11-16 08:30:55 +0100

    renamed interface methods

--- src/main/java/de/oszimt/fa45/motivoking/functionality/ProgramLogic.java
+++ src/main/java/de/oszimt/fa45/motivoking/functionality/ProgramLogic.java
@@ -4,8 +4,8 @@ package de.oszimt.fa45.motivoking.functionality;
  * Created by boerg on 12.10.2016.
  */
 public interface ProgramLogic {
-    public void create();
-    public void read();
-    public void update();
-    public void delete();
+    void create();
+    void read();
+    void update();
+    void delete();
 }
