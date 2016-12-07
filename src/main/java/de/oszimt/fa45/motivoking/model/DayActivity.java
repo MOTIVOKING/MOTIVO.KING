3e3152f44dd9c10c1847f76edceb8d188f37893a
Børge Grunicke <boerge@grunicke.de>
2016-12-07 14:21:33 +0100

    added classdiagram generated from intellij

--- src/main/java/de/oszimt/fa45/motivoking/model/DayActivity.java
+++ src/main/java/de/oszimt/fa45/motivoking/model/DayActivity.java
@@ -1,8 +1,13 @@
 package de.oszimt.fa45.motivoking.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by RedCyberSamurai on 28.11.2016.
  */
+@Entity
+@Table(name = "dayactivities")
 public class DayActivity extends Model {
 
     private long activityId;
