05b37203b5558e878c6f5af11f9c16cd518b364b
RedCyberSamurai <not@available.de>
2016-12-08 01:15:05 +0100

    new methods group by and inner join

--- src/main/java/de/oszimt/fa45/motivoking/data/sqlite/QueryBuilder.java
+++ src/main/java/de/oszimt/fa45/motivoking/data/sqlite/QueryBuilder.java
@@ -38,6 +38,17 @@ public class QueryBuilder {
         return this;
     }
 
+    public QueryBuilder innerJoin(String table) {
+
+        query += " INNER JOIN " + table;
+        return this;
+    }
+
+    public QueryBuilder on(String target1, String target2) {
+
+        query += " ON " + target1 + "=" + target2;
+        return this;
+    }
 
     public QueryBuilder where(String target1, String delimiter, String target2) {
         query += " WHERE " + target1 + delimiter + target2;
@@ -54,6 +65,12 @@ public class QueryBuilder {
     public QueryBuilder orderBy(String order) {
 
         query += " ORDER_BY " + order;
+        return this;
+    }
+
+    public QueryBuilder groupBy(String order) {
+
+        query += " GROUP BY " + order;
         return this;
     }
 
